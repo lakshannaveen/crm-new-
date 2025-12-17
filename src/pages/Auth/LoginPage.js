b38124a6f923c0c6d6eee22179f9e810c653f8bb
lakshannaveen <139128804+lakshannaveen@users.noreply.github.com>
2025-12-17 11:49:32 +0530

    blobs remove

--- src/pages/Auth/LoginPage.js
+++ src/pages/Auth/LoginPage.js
@@ -8,7 +8,6 @@ const LoginPage = () => {
 
       {/* Background Blobs */}
       <div className="absolute inset-0 pointer-events-none">
-        <div className="absolute -top-32 -right-32 w-96 h-96 bg-blue-300/30 dark:bg-blue-900/30 rounded-full blur-3xl" />
         <div className="absolute -bottom-32 -left-32 w-96 h-96 bg-purple-300/20 dark:bg-purple-900/30 rounded-full blur-3xl" />
       </div>
 
--- src/pages/Auth/RegisterPage.js
+++ src/pages/Auth/RegisterPage.js
@@ -8,7 +8,6 @@ const RegisterPage = () => {
 
       {/* Background Blobs - Same as login page */}
       <div className="absolute inset-0 pointer-events-none">
-        <div className="absolute -top-32 -right-32 w-96 h-96 bg-blue-300/30 dark:bg-blue-900/30 rounded-full blur-3xl" />
         <div className="absolute -bottom-32 -left-32 w-96 h-96 bg-purple-300/20 dark:bg-purple-900/30 rounded-full blur-3xl" />
       </div>
 
