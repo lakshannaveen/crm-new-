71a0d5eb2418ec7db694a6214f8e98ecd7a5eff7
Achira Senadheera <163681107+AchiraUwanpriya@users.noreply.github.com>
2026-01-05 15:18:29 +0530

    Comment out error message for date fetch failure
    Temporarily disables the UI warning shown when date fetching fails. This may be for debugging or to prevent user confusion while the underlying issue is addressed.

--- src/components/feedback/FeedbackForm.js
+++ src/components/feedback/FeedbackForm.js
@@ -1380,13 +1380,13 @@ const FeedbackForm = ({ vessel, onSubmit }) => {
               </div>
 
               {/* Show error if dates fetch fails */}
-              {datesError && (
+              {/* {datesError && (
                 <div className="p-3 bg-yellow-50 dark:bg-yellow-900/20 border border-yellow-200 dark:border-yellow-800 rounded-lg">
                   <p className="text-sm text-yellow-800 dark:text-yellow-200">
                     Note: Could not auto-load dates. Please enter them manually.
                   </p>
                 </div>
-              )}
+              )} */}
 
               {/* Status Fields */}
               <div
