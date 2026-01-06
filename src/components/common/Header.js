918c3e4cec95fc1f42330c8619b935fb2065a1fe
lakshannaveen <139128804+lakshannaveen@users.noreply.github.com>
2026-01-06 14:57:31 +0530

    Update Header.js

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -271,14 +271,14 @@ const Header = () => {
               )}
             </button>
 
-            {/* Notifications */}
+            {/* Notifications
             <button
               className="p-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 relative transition-colors"
               aria-label="Notifications"
             >
               <FiBell className="w-5 h-5 text-gray-600 dark:text-gray-300" />
               <span className="absolute top-1 right-1 h-2 w-2 bg-red-500 rounded-full"></span>
-            </button>
+            </button> */}
 
             {/* Profile Menu */}
             <div className="relative">
